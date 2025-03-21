e500a68b43eaacf8bf5bc4835d2e71c3ab365b44
shBLOCK <shblock3@gmail.com>
2025-03-21 12:34:53 +0800

    Optimize `AstroTime`

--- src/commonMain/kotlin/utils/AstroTime.kt
+++ src/commonMain/kotlin/utils/AstroTime.kt
@@ -1,9 +1,9 @@
+@file:Suppress("NOTHING_TO_INLINE", "OVERRIDE_BY_INLINE", "ConstPropertyName")
+
 package utils
 
 import kotlin.math.floor
-
-/** 100 * 365.25 * 24 * 3600 */
-private const val SECONDS_IN_CENTURY = 3155760000L
+import kotlin.math.nextDown
 
 /**
  * Represents an **immutable** astronomical time in seconds relative to J2000.0
@@ -14,30 +14,39 @@ class AstroTime(
     seconds: Long,
     fraction: Double
 ) {
-    private val _seconds = seconds + floor(fraction).toLong()
-    private val _fraction = fraction.mod(1.0)
+    val seconds = seconds + floor(fraction).toLong()
+    val fraction = fraction.mod(1.0)
 
     constructor(t: Double) : this(0L, t)
     constructor(t: Long) : this(t, 0.0)
 
-    val nearestSecond get() = if (_fraction >= 0.5) _seconds + 1 else _seconds
-    val centuries get() = _seconds / SECONDS_IN_CENTURY +
-            (_fraction + _seconds.mod(SECONDS_IN_CENTURY)) / SECONDS_IN_CENTURY
+    inline val nearestSecond get() = if (fraction >= 0.5) seconds + 1 else seconds
+    inline val centuries
+        get() = seconds / SECONDS_IN_CENTURY +
+            (fraction + seconds.mod(SECONDS_IN_CENTURY)) / SECONDS_IN_CENTURY
 
-    operator fun plus(delta: Double) =
-        AstroTime(_seconds, _fraction + delta)
+    inline operator fun plus(delta: Double) =
+        AstroTime(seconds, fraction + delta)
 
-    operator fun minus(delta: Double) = plus(-delta)
+    inline operator fun minus(delta: Double) = plus(-delta)
 
-    override operator fun equals(other: Any?) =
+    override inline operator fun equals(other: Any?) =
         if (other is AstroTime) {
-            _seconds == other._seconds
-                && _fraction == other._fraction
+            seconds == other.seconds
+                && fraction == other.fraction
         } else false
 
-    operator fun compareTo(other: AstroTime) =
-        _seconds.compareTo(other._seconds)
-            .let { if (it == 0) _fraction.compareTo(other._fraction) else it }
+    inline operator fun compareTo(other: AstroTime) =
+        seconds.compareTo(other.seconds)
+            .let { if (it == 0) fraction.compareTo(other.fraction) else it }
 
-    override fun hashCode() = _seconds.hashCode() xor centuries.hashCode()
+    override fun hashCode() = seconds.hashCode() xor centuries.hashCode()
+
+    companion object {
+        val MIN = AstroTime(Long.MIN_VALUE, 0.0)
+        val MAX = AstroTime(Long.MAX_VALUE, 1.0.nextDown())
+
+        /** 100 * 365.25 * 24 * 3600 */
+        const val SECONDS_IN_CENTURY = 3155760000L
+    }
 }
