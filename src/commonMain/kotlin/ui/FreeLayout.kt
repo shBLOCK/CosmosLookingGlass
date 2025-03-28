83dc381b5eead446d4a04d1a5a8aa686f729f532
shBLOCK <shblock3@gmail.com>
2025-03-28 11:33:39 +0800

    Move free layout extensions to `FreeLayout.kt`

--- src/commonMain/kotlin/ui/FreeLayout.kt
+++ src/commonMain/kotlin/ui/FreeLayout.kt
@@ -1,11 +1,15 @@
 package ui
 
 import de.fabmax.kool.KoolContext
+import de.fabmax.kool.math.Vec2d
 import de.fabmax.kool.modules.ui2.AlignmentX
 import de.fabmax.kool.modules.ui2.AlignmentY
 import de.fabmax.kool.modules.ui2.Dp
 import de.fabmax.kool.modules.ui2.Layout
+import de.fabmax.kool.modules.ui2.UiModifier
 import de.fabmax.kool.modules.ui2.UiNode
+import de.fabmax.kool.modules.ui2.align
+import de.fabmax.kool.modules.ui2.margin
 
 object FreeLayout : Layout {
     override fun measureContentSize(uiNode: UiNode, ctx: KoolContext) = uiNode.run {
@@ -32,3 +36,14 @@ object FreeLayout : Layout {
         }
     }
 }
+
+fun UiModifier.free(
+    x: Double, y: Double,
+    alignX: AlignmentX = AlignmentX.Start, alignY: AlignmentY = AlignmentY.Top
+) {
+    margin(start = x.dpx, top = y.dpx, end = Dp.ZERO, bottom = Dp.ZERO)
+    align(alignX, alignY)
+}
+
+fun UiModifier.free(pos: Vec2d, alignX: AlignmentX = AlignmentX.Start, alignY: AlignmentY = AlignmentY.Top) =
+    free(pos.x, pos.y, alignX, alignY)
