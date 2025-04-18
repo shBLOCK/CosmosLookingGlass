a17f99116fa021caa9da288b7aeddd05a9a56bdb
shBLOCK <shblock3@gmail.com>
2025-04-18 13:25:31 +0800

    Auto-name `SingletonCelestialBody`

--- src/commonMain/kotlin/universe/SingletonCelestialBody.kt
+++ src/commonMain/kotlin/universe/SingletonCelestialBody.kt
@@ -4,6 +4,9 @@ import utils.Companioned
 
 abstract class SingletonCelestialBody<Self : SingletonCelestialBody<Self, CP>, CP : SingletonCelestialBody.CompanionObj<Self>> :
     CelestialBody(), Companioned<CP> {
+    init {
+        name = this::class.simpleName ?: makeNodeName(SingletonCelestialBody::class.simpleName!!)
+    }
 
     abstract class CompanionObj<@Suppress("unused") CLS : SingletonCelestialBody<*, *>>
 }
