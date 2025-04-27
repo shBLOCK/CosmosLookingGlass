487a3256cc010f2d3c15cc821f247a7e9c98a11d
shBLOCK <shblock3@gmail.com>
2025-04-27 11:29:16 +0800

    Use cubemap textures for all celestial bodies

--- src/commonMain/kotlin/universe/content/CelestialBodies.kt
+++ src/commonMain/kotlin/universe/content/CelestialBodies.kt
@@ -7,7 +7,11 @@ class Mercury : SingletonCelestialBody<Mercury, Mercury.Companion>() {
     override val outlineRadius = SolarSystemConsts.MERCURY_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.MERCURY_RADIUS, SolarSystemConsts.MERCURY_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.MERCURY_RADIUS, SolarSystemConsts.MERCURY_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.MERCURY_RADIUS, SolarSystemConsts.MERCURY_FLATTENING,
+            "textures/celestial_body/mercury/color.png"
+        )
     }
 
     companion object : CompanionObj<Mercury>();
@@ -19,7 +23,11 @@ class Venus : SingletonCelestialBody<Venus, Venus.Companion>() {
     override val outlineRadius = SolarSystemConsts.VENUS_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.VENUS_RADIUS, SolarSystemConsts.VENUS_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.VENUS_RADIUS, SolarSystemConsts.VENUS_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.VENUS_RADIUS, SolarSystemConsts.VENUS_FLATTENING,
+            "textures/celestial_body/venus/color.png"
+        )
     }
 
     companion object : CompanionObj<Venus>();
@@ -31,7 +39,11 @@ class Mars : SingletonCelestialBody<Mars, Mars.Companion>() {
     override val outlineRadius = SolarSystemConsts.MARS_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.MARS_RADIUS, SolarSystemConsts.MARS_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.MARS_RADIUS, SolarSystemConsts.MARS_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.MARS_RADIUS, SolarSystemConsts.MARS_FLATTENING,
+            "textures/celestial_body/mars/color.png"
+        )
     }
 
     companion object : CompanionObj<Mars>();
@@ -43,7 +55,11 @@ class Jupiter : SingletonCelestialBody<Jupiter, Jupiter.Companion>() {
     override val outlineRadius = SolarSystemConsts.JUPITER_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.JUPITER_RADIUS, SolarSystemConsts.JUPITER_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.JUPITER_RADIUS, SolarSystemConsts.JUPITER_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.JUPITER_RADIUS, SolarSystemConsts.JUPITER_FLATTENING,
+            "textures/celestial_body/jupiter/color.png"
+        )
     }
 
     companion object : CompanionObj<Jupiter>();
@@ -55,7 +71,11 @@ class Saturn : SingletonCelestialBody<Saturn, Saturn.Companion>() {
     override val outlineRadius = SolarSystemConsts.SATURN_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.SATURN_RADIUS, SolarSystemConsts.SATURN_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.SATURN_RADIUS, SolarSystemConsts.SATURN_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.SATURN_RADIUS, SolarSystemConsts.SATURN_FLATTENING,
+            "textures/celestial_body/saturn/color.png"
+        )
     }
 
     companion object : CompanionObj<Saturn>();
@@ -67,7 +87,11 @@ class Uranus : SingletonCelestialBody<Uranus, Uranus.Companion>() {
     override val outlineRadius = SolarSystemConsts.URANUS_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.URANUS_RADIUS, SolarSystemConsts.URANUS_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.URANUS_RADIUS, SolarSystemConsts.URANUS_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.URANUS_RADIUS, SolarSystemConsts.URANUS_FLATTENING,
+            "textures/celestial_body/uranus/color.png"
+        )
     }
 
     companion object : CompanionObj<Uranus>();
@@ -79,7 +103,11 @@ class Neptune : SingletonCelestialBody<Neptune, Neptune.Companion>() {
     override val outlineRadius = SolarSystemConsts.NEPTUNE_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.NEPTUNE_RADIUS, SolarSystemConsts.NEPTUNE_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.NEPTUNE_RADIUS, SolarSystemConsts.NEPTUNE_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.NEPTUNE_RADIUS, SolarSystemConsts.NEPTUNE_FLATTENING,
+            "textures/celestial_body/neptune/color.png"
+        )
     }
 
     companion object : CompanionObj<Neptune>();
@@ -91,7 +119,11 @@ class Moon : SingletonCelestialBody<Moon, Moon.Companion>() {
     override val outlineRadius = SolarSystemConsts.MOON_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.MOON_RADIUS, SolarSystemConsts.MOON_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.MOON_RADIUS, SolarSystemConsts.MOON_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.MOON_RADIUS, SolarSystemConsts.MOON_FLATTENING,
+            "textures/celestial_body/moon/color.png"
+        )
     }
 
     companion object : CompanionObj<Moon>();
@@ -103,7 +135,11 @@ class Sun : SingletonCelestialBody<Sun, Sun.Companion>() {
     override val outlineRadius = SolarSystemConsts.SUN_RADIUS
 
     init {
-        setupSimpleSpherical(SolarSystemConsts.SUN_RADIUS, SolarSystemConsts.SUN_FLATTENING)
+//        setupSimpleSpherical(SolarSystemConsts.SUN_RADIUS, SolarSystemConsts.SUN_FLATTENING)
+        setupTexturedSpherical(
+            SolarSystemConsts.SUN_RADIUS, SolarSystemConsts.SUN_FLATTENING,
+            "textures/celestial_body/sun/color.png"
+        )
     }
 
     companion object : CompanionObj<Sun>();
